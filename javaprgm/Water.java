5392891b7a203bd4cde8bc2a50e8ae4803b2925d
Ashwinip9 <ashwiniparashuram.xworkz@gmail.com>
2024-07-26 11:29:27 +0530

    commit

---
+++ javaprgm/Water.java
@@ -0,0 +1,56 @@
+//Create below classes and create a copy
+//water
+//flour
+//salt
+//oil
+//bowl
+//pan
+//Person & Hand
+//stove
+//gas
+//roller stick
+//lighter
+//spoon
+
+class Water{
+	
+	
+}
+class Flour{
+	
+	
+}
+class Salt{
+	
+}
+
+class Oil{
+	
+}
+
+class Bowl{
+	
+	
+}
+class Pan{
+	
+}
+class Hand{
+	
+	
+}
+class Stove{
+	
+	
+}
+class Spoon{
+	
+}
+class Gas{
+	
+	
+}
+class Lighter{
+	
+	
+}
---
+++ javaprgm/WaterRunner.java
@@ -0,0 +1,31 @@
+class WaterRunner{
+public static void main(String[] args){
+System.out.println("start main in waterRunner");
+new Water( );
+new Salt( );
+new Oil( );
+new Spoon( );
+new Stove( );
+new Hand( );
+new Gas( );
+new Lighter( );
+new Bowl( );
+
+System.out.println("end main in waterRunner");
+
+}
+
+}
+
+
+//flour
+//salt
+//oil
+//bowl
+//pan
+//Person & Hand
+//stove
+//gas
+//roller stick
+//lighter
+//spoon
