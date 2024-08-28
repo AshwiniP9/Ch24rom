7ad862a634bb32995bab1187dc5fd5a9d775ed4b
Ashwinip9 <ashwiniparashuram.xworkz@gmail.com>
2024-08-28 15:12:43 +0530

    28

---
+++ javaprgm/taskon28/src/com/xworkz/inheritence/things/Bulb.java
@@ -0,0 +1,16 @@
+package com.xworkz.inheritence.things;
+
+
+public class Bulb {
+
+    public String brand;
+
+    public Bulb(){
+        System.out.println("constructor of Bulb Created");
+    }
+    public void buy()
+    {
+        System.out.println("Buy method created");
+    }
+
+}
